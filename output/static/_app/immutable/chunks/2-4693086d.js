c52e1580f4e6a7e90e3fd18f5f8855adebf11885
Rizali Ahmad <rizaliahmd@gmail.com>
2023-02-15 15:38:42 +0700

    Server load

---
+++ output/static/_app/immutable/chunks/2-4693086d.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-5d0e02ac.js";export{t as component};
--- output/static/_app/immutable/chunks/2-8cb10681.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./_page-27c769fd.js";import{default as t}from"../components/pages/_page.svelte-5d0e02ac.js";export{t as component,r as universal};
