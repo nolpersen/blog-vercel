25654d46768491f308b00373792ae6b2b170e324
Rizali Ahmad <rizaliahmd@gmail.com>
2023-02-15 12:07:55 +0700

    New Build Fix again

--- output/static/_app/immutable/chunks/2-8442d2dc.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./_page-76ee5a52.js";import{default as t}from"../components/pages/_page.svelte-63c6a801.js";export{t as component,r as universal};
---
+++ output/static/_app/immutable/chunks/2-f0bc11aa.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./_page-76ee5a52.js";import{default as t}from"../components/pages/_page.svelte-c9dd1a77.js";export{t as component,r as universal};
