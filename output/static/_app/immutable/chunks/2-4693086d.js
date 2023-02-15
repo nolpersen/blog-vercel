963bc2a66f11e40623b559cc8becdbad264201c1
Rizali Ahmad <rizaliahmd@gmail.com>
2023-02-15 15:41:18 +0700

    Search bar

--- output/static/_app/immutable/chunks/2-4693086d.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-5d0e02ac.js";export{t as component};
---
+++ output/static/_app/immutable/chunks/2-79c22800.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-c2f8788e.js";export{t as component};
